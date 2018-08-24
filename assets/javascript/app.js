0b3484794882ce7a0e94b4c24660fe3c203bd6db
sydneycode <sstaggs12@gmail.com>
2018-08-23 19:23:36 -0700

    finished the logic necessary to move from question to answer to next question, etc.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,17 @@ var questionBank = [
     {
         question: "Question 1",
         answers: ["a", "b", "c", "d"],
-        correctAnswer: "a"
+        correctAnswer: 0
     },
     {
         question: "Question 2",
         answers: ["a", "b", "c", "d"],
-        correctAnswer: "b"
+        correctAnswer: 1
     }
 ]
 
 window.onload = function() {
-    $('#start-button').on("click", game.startQuiz);
+    $('#start-button').on("click", game.startQuestion);
 }
 
 var intervalId;
@@ -27,14 +27,18 @@ var game = {
     time: 0,
     questionIndex: 0,
     score: 0,
+    numIncorrect: 0,
+    numUnanswered: 0,
     lastCorrect: false,
+    timedOut: false,
 
-    startQuiz: function() {
+    startQuestion: function() {
         var current = questionBank[game.questionIndex];
         $('#trivia').html("<p>Time Remaining: <span id='time-rem'>" + game.questionWait + "</span> Seconds</p>");
-        $('#trivia').append("<p>" + current.question + "</p>");
+        $('#trivia').append("<p id='question'>" + current.question + "</p>");
+        $('#trivia').append("<div id='answers'><div>")
         for (var i = 0; i < current.answers.length; i++) {
-            $('#trivia').append("<button id='button" + i + "'>" + current.answers[i] + "</button>");
+            $('#answers').append("<button id='button" + i + "'>" + current.answers[i] + "</button>");
         }
 
         $('#button0').on("click", game.getAnswer0);
@@ -55,14 +59,46 @@ var game = {
         }
     },
 
+    startClockForAnswer: function() {
+        if (!clockTicking) {
+          game.time = game.answerWait;
+          intervalId = setInterval(game.countDownAnswer, 1000);
+          clockTicking = true;
+        }
+    },
+
     countDown: function() {
         console.log('counting');
         game.time--;
-        $('#time-rem').text(game.time);
+        console.log(game.time);
+        $('#time-rem').html(game.time);
 
         if (game.time === 0) {
             // stop counting down
             game.stopClock();
+            game.timedOut = true;
+            game.numUnanswered++;
+            game.showAnswer();
+            game.startClockForAnswer();
+        }
+    },
+
+    countDownAnswer: function() {
+        console.log('counting');
+        game.time--;
+        console.log(game.time);
+        
+        if (game.time === 0) {
+            // stop counting down
+            game.stopClock();
+            game.questionIndex++;
+            if (game.questionIndex < questionBank.length) {
+                game.startQuestion();
+            }
+            else {
+                game.showQuizResults();
+            }
+            
         }
     },
 
@@ -73,67 +109,112 @@ var game = {
 
     getAnswer0: function() {
         var current = questionBank[game.questionIndex];
-        if (current.correctAnswer === 'a') {
+        if (current.correctAnswer === 0) {
             game.score++;
             game.lastCorrect = true;
         }
         else {
-            lastCorrect = false;
+            game.lastCorrect = false;
+            game.numIncorrect++;
         }
         game.stopClock();
         game.showAnswer();
+        game.startClockForAnswer();
     },
 
     getAnswer1: function() {
         var current = questionBank[game.questionIndex];
-        if (current.correctAnswer === 'b') {
+        if (current.correctAnswer === 1) {
             game.score++;
             game.lastCorrect = true;
         }
         else {
             game.lastCorrect = false;
+            game.numIncorrect++;
         }
         game.stopClock();
         game.showAnswer();
+        game.startClockForAnswer();
     },
 
     getAnswer2: function() {
         var current = questionBank[game.questionIndex];
-        if (current.correctAnswer === 'c') {
+        if (current.correctAnswer === 2) {
             game.score++;
             game.lastCorrect = true;
         }
         else {
             game.lastCorrect = false;
+            game.numIncorrect++;
         }
         game.stopClock();
         game.showAnswer();
+        game.startClockForAnswer();
     },
 
     getAnswer3: function() {
         var current = questionBank[game.questionIndex];
-        if (current.correctAnswer === 'd') {
+        if (current.correctAnswer === 3) {
             game.score++;
             game.lastCorrect = true;
         }
         else {
             game.lastCorrect = false;
+            game.numIncorrect++;
         }
         game.stopClock();
         game.showAnswer();
+        game.startClockForAnswer();
     },
 
     showAnswer: function() {
-        if (game.lastCorrect) {
-            // let the user know that he or she picked the correct answer
-            console.log('correct');
+        if (game.timedOut) {
+            console.log('out of time');
+            $('#question').html("Out of time!");
+            $('#answers').empty();
+            var current = questionBank[game.questionIndex];
+            var correct = current.answers[current.correctAnswer];
+            $('#answers').html("The correct answer was: " + correct);
+            game.timedOut = false;
         }
         else {
-            // let the user know that he or she picked an incorrect answer and 
-            // show the user the correct answer
-            console.log('incorrect');
-
+            if (game.lastCorrect) {
+                // let the user know that he or she picked the correct answer
+                console.log('correct');
+                $('#question').html("Correct!");
+                $('#answers').empty();
+            }
+            else {
+                // let the user know that he or she picked an incorrect answer and 
+                // show the user the correct answer
+                console.log('incorrect');
+                $('#question').html("Incorrect!");
+                $('#answers').empty();
+            }
         }
+    },
+
+    showQuizResults: function() {
+        console.log('done');
+        $('#question').html("<p>Done! Here's how you did...<p>");
+        
+        $('#answers').empty();
+        $('#answers').html("<p class='stats'>Correct Answers: " + game.score + "<p>");
+        $('#answers').append("<p class='stats'>Incorrect Answers: " + game.numIncorrect + "<p>");
+        $('#answers').append("<p class='stats'>Unanswered: " + game.numUnanswered + "<p>");
+        $('#answers').append("<button id='start-over'>Start Over</button>");
+
+        $('#start-over').on("click", function() {
+            game.time = 0;
+            game.questionIndex = 0;
+            game.score = 0;
+            game.numIncorrect = 0;
+            game.numUnanswered = 0;
+            game.lastCorrect = false;
+            game.timedOut = false;
+            game.startQuestion();
+        });
+        
     }
 }
 
